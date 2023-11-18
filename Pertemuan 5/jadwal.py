43b614ffe2e11ceb0879650bc9a11423ffd7ab68
Lugas Nusa Bakti <122817340+lugasnb@users.noreply.github.com>
2023-11-18 20:25:24 +0700

    Add files via upload

---
+++ Pertemuan 5/jadwal.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def browse_file():
+    file_path = filedialog.askopenfilename()
+    with open(file_path, 'r') as file:
+        content = file.read()
+        entry.delete("1.0", tk.END)
+        entry.insert("1.0", content)
+        file.close()
+    result_label.config(text="File telah dibuka!", foreground='white')
+
+window = tk.Tk()
+window.title("Aplikasi Jadwal Pelajaran")
+window['bg'] = 'gray'
+
+entry = tk.Text(window, width=40,height=15)
+entry.grid(row=0, column=0, padx=20, pady=20)
+
+frame = tk.Frame(window)
+frame.grid(row=0, column=1, padx=10, pady=10)
+frame['bg'] = 'gray'
+
+browse_button = tk.Button(frame, text="Buka File", command=browse_file)
+browse_button.grid(row=1, column=0, padx=10, pady=10)
+
+result_label = tk.Label(frame, text="")
+result_label.grid(row=2, column=0, pady=10)
+result_label['bg'] = 'gray'
+
+
+window.mainloop()
